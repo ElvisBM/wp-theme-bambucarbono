e95bc2f1631216b9f0319af23556e463b56e7009
Elvis Martins <elvisbmartins@gmail.com>
2018-03-03 20:41:47 -0300

    style home

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
  */
 
 get_header(); ?>
-	<div id="banner">
+	<div id="banner-home">
 		<?php
 			//Galeria
 			$gallery = get_post_meta( $post->ID, 'repeatable_images', true );
@@ -14,20 +14,22 @@ get_header(); ?>
 				$count = count($gallery);
 		?>
 		<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-			<?php 
-				for ($i = 0; $i < $count; $i++) {		
-	  				$url_image = wp_get_attachment_url( $gallery[$i]['images'] ); 
-	  				$text_image = $gallery[$i]['text'];
-	  				$active = ( $i==0 ? ' active' : '' );
-	  				echo '<div class="carousel-item '.$active.'">';
-	  				echo '<div class="item" style="background: url('.$url_image.');">';
-	  				echo '<div class="container">';
-	  				echo '<h3>'.$text_image.'</h3>';
-					echo '</div>';
-		  			echo '</div>';
-		  			echo '</div>';
-			  	}
-			?>
+			<div class="carousel-inner" role="listbox">
+				<?php 
+					for ($i = 0; $i < $count; $i++) {		
+		  				$url_image = wp_get_attachment_url( $gallery[$i]['images'] ); 
+		  				$text_image = $gallery[$i]['text'];
+		  				$active = ( $i==0 ? ' active' : '' );
+		  				echo '<div class="carousel-item '.$active.'">';
+		  				echo '<div class="item" style="background: url('.$url_image.');">';
+		  				echo '<div class="container">';
+		  				echo '<h3>'.$text_image.'</h3>';
+						echo '</div>';
+			  			echo '</div>';
+			  			echo '</div>';
+				  	}
+				?>
+			</div>
 		</div>
 		<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
 	    	<span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -39,7 +41,7 @@ get_header(); ?>
 	  	</a>
 	  	<?php }//end if repeatable ?>			
 	</div>
-	<div id="metas" class="container">
+	<div id="metas-home" class="container">
 		<div class="row">			
 			<?php 
 				$metas = get_post_meta( $post->ID );
@@ -67,23 +69,27 @@ get_header(); ?>
 			?>
 		</div>
 	</div>
-	<div class="container">
+	<div id="pages-home" class="container">
 		<div class="row">
 		<?php 
 			$args = array(
 				'exclude' => '76',
 				'post_type' => 'page',
-				'post_status' => 'publish'
+				'post_status' => 'publish',
+				'sort_order' => 'asc',
+				'sort_column' => 'menu_order',
 			); 
 			$pages = get_pages($args); 
 			foreach ( $pages as $page ) {
 				$src = wp_get_attachment_image_src( get_post_thumbnail_id($page->ID), 'medium_large' );
 				$url = $src[0];
 		?>
-				<div class="col-md-4 com-sm-12">
+				<div class="col-md-4 com-sm-12 item-page">
 					<a href="<?php echo  get_page_link( $page->ID ); ?>" alt="<?php echo $page->post_title; ?>" title="<?php echo $page->post_title; ?>">
 						<img src="<?php echo $url; ?>" title="<?php echo $page->post_title; ?>" alt="<?php echo $page->post_title; ?>" />
-						<span><?php echo $page->post_title; ?></span>
+						<div class="bg-h3">
+							<h3><?php echo $page->post_title; ?></h3>
+						</div>
 					</a>
 				</div>
 		
