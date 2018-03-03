8b107677c126bfcdaf2f695d2e0f757122c1a16d
Elvis Martins <elvisbmartins@gmail.com>
2018-03-03 16:11:12 -0300

    estrutura e conteudo footer

--- footer.php
+++ footer.php
@@ -9,10 +9,56 @@
 ?>
 
 
-<footer class="aa_footer">
-	<p>This is the footer.</p>
+<footer class="footer">
+	<div id="footer">
+		<div class="container">
+			<div class="row">
+				<?php 
+					if ( is_active_sidebar( 'contato-footer' ) ) {
+						echo '<div class="contato-footer col-md-6 col-sm-12">';
+							dynamic_sidebar( 'contato-footer' );
+						echo '</div>';
+					};
+				?>
+				<?php 
+					if ( is_active_sidebar( 'infos-footer' ) ) {
+						echo '<div class="infos-footer col-md-6 col-sm-12">';
+							dynamic_sidebar( 'infos-footer' );
+						echo '</div>';
+					};
+				?>
+			</div>
+		</div>
+	</div>
+	<div id="rodape">
+		<div class="container">
+			<div class="row">
+				<?php 
+					if ( is_active_sidebar( 'menu-footer' ) ) {
+						echo '<div class="infos-footer col-md-7 col-sm-12">';
+							dynamic_sidebar( 'menu-footer' );
+						echo '</div>';
+					};
+				?>
+				<?php 
+					if ( is_active_sidebar( 'agencia-footer' ) ) {
+						echo '<div class="infos-footer col-md-3 col-sm-12">';
+							dynamic_sidebar( 'agencia-footer' );
+						echo '</div>';
+					};
+				?>
+				<?php 
+					if ( is_active_sidebar( 'social-footer' ) ) {
+						echo '<div class="social-footer col-md-2 col-sm-12">';
+							dynamic_sidebar( 'social-footer' );
+						echo '</div>';
+					};
+				?>
+			</div>
+		</div>
+	</div>
 </footer>
-<!-- /.aa_footer -->
+
 
 <?php wp_footer(); ?>
 
