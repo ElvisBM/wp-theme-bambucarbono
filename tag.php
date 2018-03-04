9429306109942ea5e6b0af6085247276a0b87da0
Elvis Martins <elvisbmartins@gmail.com>
2018-03-03 23:30:03 -0300

    estrutura page lista de tags tag.php

---
+++ tag.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The template for displaying all single posts.
+ *
+ * @package Neat
+ */
+
+get_header(); ?>
+<div id="blog">
+	<div id="banner">
+		<?php
+			$url = get_term_meta( 2, 'categoryimage', true );
+		?>
+		<div class="item" style="background: url(<?php echo $url; ?>);">
+		 	<div class="container">
+		  		<h3><?php the_archive_title();?><br /><?php the_archive_description(); ?></h3>
+		  	</div>
+		</div>
+	</div>
+	<div id="content">
+		<div class="container">	
+			<?php echo term_description( 2, $taxonomy ) ?>
+		</div>
+	</div>
+	<div  class="container">	
+		<div class="row">
+			<div id="posts" class="col-md-9 col-xs-12">
+				<?php while ( have_posts() ) : the_post(); ?>
+					<?php get_template_part( 'templates/content', 'blog' ); ?>
+					<?php //the_post_navigation(); ?>
+				<?php endwhile; // end of the loop. ?>
+			</div>
+			<div id="sidebar" class="col-md-3 hidden-sm">
+				<?php get_sidebar(); ?>
+			</div>
+		</div>
+	</div>
+
+</div>
+<?php get_footer(); ?>
