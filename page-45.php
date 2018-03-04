1080ac54fb42124d10d85a2a4a5fbcf294eacbe8
Elvis Martins <elvisbmartins@gmail.com>
2018-03-03 21:38:13 -0300

    page-45 = page clientes, por causa da galeria e colunas

---
+++ page-45.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package Neat
+ */
+
+get_header(); ?>
+
+<div id="page" class="page-clientes">
+	<div id="banner">
+		<?php
+			$url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
+		?>
+		<div class="item" style="background: url(<?php echo $url; ?>);">
+		 	<div class="container">
+		  		<h3><?php echo $post->post_title;?></h3>
+		  	</div>
+		</div>
+	</div>
+	<div id="content" >
+		<div class="container">
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php the_content(); ?>
+			<?php endwhile; // end of the loop. ?>
+		</div>
+	</div>
+	<?php
+		//Galeria
+		$gallery = get_post_meta( $post->ID, 'repeatable_images', true );
+		if(! empty($gallery) ){
+				$count = count($gallery);
+	?>
+	<div id="galeria">
+		<div class="container">
+			<div class="row">
+				<?php
+					foreach ( $gallery as $ga ) {
+						$url_image = wp_get_attachment_url( $ga['images']); 
+		  				$text_image = $ga['text'];
+		  				echo '<div class="col-md-3 com-sm-12 item-page">';
+		  				echo '<img src="'.$url_image.'" />';
+		  				echo '<div class="bg-h3"><h3>'.$text_image.'</h3></div>';
+		  				echo '</div>';
+					}
+				?>
+			</div>
+		</div>
+	</div>
+	<?php
+		}//EndIf empty gallery
+	?>
+</div>
+
+<?php get_footer(); ?>
+
+
