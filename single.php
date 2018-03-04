d1176fcb391c8b4c4ff6a57a50414d140802e3c7
Elvis Martins <elvisbmartins@gmail.com>
2018-03-03 23:50:26 -0300

    estrutura e style single blog

--- single.php
+++ single.php
@@ -6,27 +6,33 @@
  */
 
 get_header(); ?>
-
-	<div class="aa_single">
-
-		<?php while ( have_posts() ) : the_post(); ?>
-
-			<?php get_template_part( 'templates/content', 'single' ); ?>
-
-			<?php the_post_navigation(); ?>
-
-			<?php
-				// If comments are open or we have at least one comment, load up the comment template
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-			?>
-
-		<?php endwhile; // end of the loop. ?>
-
-
+<div id="blog">
+	<div id="banner">
+		<?php
+			$url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
+			// Get the URL of this category
+   			 $cat_url = get_category_link( 2 );
+		?>
+		<div class="item" style="background: url(<?php echo $url; ?>);">
+		 	<div class="container">
+		  		<h3><a href="<?php echo esc_url( $cat_url ); ?>" title="Blog Bambu Carbono Zero" class="bloglink">Blog</a> >> <?php echo $post->post_title;?></h3>
+		  	</div>
+		</div>
 	</div>
-	<!-- /.aa_single -->
+	<div  class="container">	
+		<div class="row">
+			<div id="posts" class="col-md-9 col-xs-12">
+				<?php while ( have_posts() ) : the_post(); ?>
+					<h3><?php the_title(); ?></h3>
+					<?php the_content(); ?>
+					<?php the_post_thumbnail( 'medium_large' ); ?>
+				<?php endwhile; // end of the loop. ?>
+			</div>
+			<div id="sidebar" class="col-md-3 hidden-sm">
+				<?php get_sidebar(); ?>
+			</div>
+		</div>
+	</div>
 
-<?php get_sidebar(); ?>
+</div>
 <?php get_footer(); ?>
